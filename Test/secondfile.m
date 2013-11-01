d108f9c23a5b09b8ab5abdf65e7e5d7a0b51e7f0
suezeng <zengsuyan@gmail.com>
2013-10-31 22:49:59 -0400

    second file

---
+++ Test/secondfile.m
@@ -0,0 +1,13 @@
+//
+//  secondfile.m
+//  Test
+//
+//  Created by suezeng on 10/31/2013.
+//  Copyright (c) 2013 suezeng. All rights reserved.
+//
+
+#import "secondfile.h"
+
+@implementation secondfile
+
+@end
