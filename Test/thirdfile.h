e60b475a6b789c19933658e314f8c8ecd3dfa125
suezeng <zengsuyan@gmail.com>
2013-10-31 22:54:59 -0400

    third file

---
+++ Test/thirdfile.h
@@ -0,0 +1,13 @@
+//
+//  thirdfile.h
+//  Test
+//
+//  Created by suezeng on 10/31/2013.
+//  Copyright (c) 2013 suezeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface thirdfile : NSObject
+
+@end
---
+++ Test/thirdfile.m
@@ -0,0 +1,13 @@
+//
+//  thirdfile.m
+//  Test
+//
+//  Created by suezeng on 10/31/2013.
+//  Copyright (c) 2013 suezeng. All rights reserved.
+//
+
+#import "thirdfile.h"
+
+@implementation thirdfile
+
+@end
