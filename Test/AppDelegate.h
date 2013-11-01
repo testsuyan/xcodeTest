72a2cb042fb0e07be628ec09298403d3b2233b06
suezeng <zengsuyan@gmail.com>
2013-10-31 22:25:39 -0400

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by suezeng on 10/31/2013.
+//  Copyright (c) 2013 suezeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by suezeng on 10/31/2013.
+//  Copyright (c) 2013 suezeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
